ae8386a1fbf7d04e919e37290d4e6a0522ca4605
mohamedirfan <mohamedirfan@gmail.com>
2020-07-04 21:58:34 +0530

    create pythonscript1
    created first script

---
+++ Lab01.py
@@ -0,0 +1,33 @@
+
+#Python is a case sensitive language
+
+print("Welcome to Python")
+
+#single line comments
+
+
+print("""
+Multi 
+line 
+comments
+""")
+
+'''This is also a
+     multiline
+     comment.'''
+
+
+#dynamic typing
+a = 5
+print(type(a))
+
+a = "abc"
+print(type(a))
+
+#strongly type
+
+b = 123
+
+#c = a + b
+
+#print(c)
