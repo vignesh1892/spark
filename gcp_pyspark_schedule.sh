86a5fc1947c31849d425f92de0658a3b04c9b0a3
mohamedirfan <mohamedirfan@gmail.com>
2023-08-24 23:14:43 +0530

    Add files via upload

---
+++ gcp_pyspark_schedule.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 /home/ashfaqalamlearning/.git/spark/Usecase4_GcpGcsReadWritehive_cloud.py
+if [ $? -ne 0 ]
+then
+echo "`date` error occured in the Pyspark job" &> /tmp/gcp_pyspark_schedule.log
+else
+echo "`date` Pyspark job is completed successfully" &> /tmp/gcp_pyspark_schedule.log
+fi
+echo "`date` gcloud hive ETL script is completed" &> /tmp/gcp_pyspark_schedule.log
