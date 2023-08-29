622b124d7edb275990d77dc45b4eb313aa507ad1
vignesh1892 <43288360+vignesh1892@users.noreply.github.com>
2023-08-29 12:27:52 +0530

    Update gcp_pyspark_schedule.sh

--- gcp_pyspark_schedule.sh
+++ gcp_pyspark_schedule.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 /home/ashfaqalamlearning/.git/spark/Usecase4_GcpGcsReadWritehive_cloud.py
+gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 /home/vigneshkumar180992/.git/spark/Usecase4_GcpGcsReadWritehive_cloud.py
 if [ $? -ne 0 ]
 then
-echo "`date` error occured in the Pyspark job" &> /tmp/gcp_pyspark_schedule.log
+echo "`date` error occured in the Pyspark job" > /tmp/gcp_pyspark_schedule.log
 else
-echo "`date` Pyspark job is completed successfully" &> /tmp/gcp_pyspark_schedule.log
+echo "`date` Pyspark job is completed successfully" > /tmp/gcp_pyspark_schedule.log
 fi
-echo "`date` gcloud hive ETL script is completed" &> /tmp/gcp_pyspark_schedule.log
+echo "`date` gcloud pyspark ETL script is completed" >> /tmp/gcp_pyspark_schedule.log
